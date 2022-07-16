0b0542a187cdb7f4ac627cb0492b8947d80370d0
Emily Santos <eyssantos@gmail.com>
2022-07-16 14:36:49 +0100

    Add some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #READ(all)
+  get "tasks", to: "tasks#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #CREATE
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  #READ(one)
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  #UPDATE
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+
+  #DELETE
+  delete "tasks/:id", to: "tasks#destroy"
+
 end
